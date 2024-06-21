d16392118fab5cf57195d0f1bee0ef908c5bf481
eliot-christon <eliot.christon@gmail.com>
2024-06-21 14:02:43 +0200

    implementing Game controller

--- src/Controller.py
+++
@@ -1,5 +0,0 @@
-__author__ = 'Eliot Christon'
-__email__ = 'eliot.christon@gmail.com'
-
-from .model.Game import Game
-from .view.Graphics import Graphics
---
+++ src/GameController.py
@@ -0,0 +1,23 @@
+__author__ = 'Eliot Christon'
+__email__ = 'eliot.christon@gmail.com'
+
+from .model.Game import Game
+from .view.Graphics import Graphics
+
+class GameController:
+    """GameController class for the Skull King game"""
+
+    def __init__(self, game:Game, graphics:Graphics) -> None:
+        self.__game     = game
+        self.__graphics = graphics
+    
+    def run(self) -> None:
+        """Play the game"""
+        while self.__game.running:
+            try:
+                self.__graphics.render()
+                self.__game.game_step()
+            except KeyboardInterrupt:
+                break
+        
+    
