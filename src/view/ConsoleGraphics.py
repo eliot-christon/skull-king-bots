51bcdd6867ab6c791bab1956604b382c3606afac
eliot-christon <eliot.christon@gmail.com>
2024-06-21 14:25:24 +0200

    better graphics

--- src/view/ConsoleGraphics.py
+++ src/view/ConsoleGraphics.py
@@ -2,30 +2,31 @@ __author__ = 'Eliot Christon'
 __email__ = 'eliot.christon@gmail.com'
 
 from .Graphics import Graphics
+from ..model.Game import Game
+
+import os
 
 
 class ConsoleGraphics(Graphics):
     """ConsoleGraphics class for the Skull King game"""
 
-    def __init__(self) -> None:
-        super().__init__()
+    def __init__(self, game:Game) -> None:
+        super().__init__(game=game)
 
-    def draw(self) -> None:
-        """Draw the graphics"""
-        pass
+    def render(self) -> None:
+        """Render the game in the console"""
+        aff = "\n" * 10
+        aff += f"Round:   {self._game.round}, state: {self._game.current_state}\n"
+        aff += f"Trick:   {self._game.trick}\n"
+        aff += f"{self.players_str()}\n"
+        print(aff, flush=True, end="")
 
-    def update(self) -> None:
-        """Update the graphics"""
-        pass
-
-    def get_input(self) -> None:
-        """Get input"""
-        pass
-
-    def clear(self) -> None:
-        """Clear the graphics"""
-        pass
-
-    def close(self) -> None:
-        """Close the graphics"""
-        pass
+    def players_str(self) -> str:
+        """Render the players in the console"""
+        spacing = [15, 5, 6]
+        res = "Player name    |Score |Tricks |Hand\n------------------------------------\n"
+        for player in self._game.players:
+            for i, current_str in enumerate([player.name, str(player.score), f"{player.tricks} / {player.bet}"]):
+                res += current_str + " " * (spacing[i] - len(current_str)) + "| "
+            res += player.hand.__str__() + "\n"
+        return res
--- src/view/Graphics.py
+++ src/view/Graphics.py
@@ -1,37 +1,20 @@
 __author__ = 'Eliot Christon'
 __email__ = 'eliot.christon@gmail.com'
 
+from ..model.Game import Game
+
 from abc import ABC, abstractmethod
+import time
 
 
 class Graphics(ABC):
     """Abstract class for graphics"""
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, game:Game) -> None:
+        self._game = game
 
     @abstractmethod
-    def draw(self) -> None:
-        """Draw the graphics"""
-        pass
-
-    @abstractmethod
-    def update(self) -> None:
-        """Update the graphics"""
-        pass
-
-    @abstractmethod
-    def get_input(self) -> None:
-        """Get input"""
-        pass
-
-    @abstractmethod
-    def clear(self) -> None:
-        """Clear the graphics"""
-        pass
-
-    @abstractmethod
-    def close(self) -> None:
-        """Close the graphics"""
+    def render(self) -> None:
+        """Render the graphics"""
         pass
 
