c2918716478bf006c6fd9a2304ee053c4bb393cd
eliot-christon <eliot.christon@gmail.com>
2024-06-21 14:25:09 +0200

    adding delay to computer player

--- src/model/players/ComputerPlayer.py
+++ src/model/players/ComputerPlayer.py
@@ -5,18 +5,22 @@ from .Player import Player
 from ..cards.Card import Card
 
 import random
+import time
 
 
 class ComputerPlayer(Player):
     """ComputerPlayer class for the Skull King game"""
 
-    def __init__(self, name:str) -> None:
+    def __init__(self, name:str, delay:float=0.1) -> None:
         super().__init__(name=name)
+        self._delay = delay
     
     def choose_card(self, requested_color:str) -> "Card":
+        time.sleep(self._delay)
         if len(self.hand.cards_of_color(requested_color)) > 0:
             return self.hand.cards_of_color(requested_color)[0]
         return self.hand[0]
     
     def place_bet(self) -> int:
+        time.sleep(self._delay)
         return random.randint(0, len(self.hand))
